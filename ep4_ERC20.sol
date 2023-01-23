0cf300f0b8eeb3743fda1361a49fea6d4ade4064
TH <blockchainjunkiez@gmail.com>
2023-01-23 19:23:58 +0800

    Update ep4_ERC20.sol

--- ep4_ERC20.sol
+++ ep4_ERC20.sol
@@ -1,10 +1,11 @@
 // SPDX-License-Identifier: MIT
+// BlockchainJunkiez : https://www.youtube.com/channel/UCEV2dnVVEe5bDB0JKVU330A/
 pragma solidity ^0.8.4;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract TH_Token is ERC20 {
-    constructor() ERC20("HING coin", "HING ") {
+    constructor() ERC20("TH Coin", "THC ") {
         _mint(msg.sender, 100 * 10 ** decimals());
     }
 }
